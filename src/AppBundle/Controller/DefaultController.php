b3bc8aef6e4e79824ff7f1427e001aef5e3be5b2
Gert <gert@gertrietveld.com>
2016-06-18 00:55:21 +0200

    extracted class from controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,132 +6,48 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use ONGR\ElasticsearchDSL\Search;
-use ONGR\ElasticsearchDSL\Query\BoolQuery;
-use ONGR\ElasticsearchDSL\Query\MatchQuery;
-use ONGR\ElasticsearchBundle\Result\Result;
-use AppBundle\Document\Item;
+use AppBundle\Matcher\Matcher;
 
 class DefaultController extends Controller
 {
     /**
-     * @var array
-     */
-    private $categories = [
-       "neo"=>"Neo",
-       "tri"=>"Trinity",
-       "cyp"=>"Cypher",
-       "mor"=>"Morpheus",
-       "tan"=>"Tank",
-    ];
-
-    /**
-     * @var array
-     */
-    private $weekdays = [
-        "sun"=>"Sunday",
-        "mon"=>"Monday",
-        "tue"=>"Tuesday",
-        "wed"=>"Wednesday",
-        "thu"=>"Thursday",
-        "fri"=>"Friday",
-        "sat"=>"Saturday",
-    ];
-
-    /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
         ]);
     }
 
     /**
      * @Route("/match", name="matchGet")
      * @Method({"GET"})
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function matchGetAction(Request $request){
+    public function matchGetAction(Request $request)
+    {
         return $this->render('match/form.html.twig');
     }
 
     /**
      * @Route("/match", name="matchPost")
      * @Method({"POST"})
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function matchPostAction(Request $request)
     {
-        $manager = $this->get('es.manager');
-        $repo = $manager->getRepository('AppBundle:Item');
-
-        $search = new Search();
-
-        $filters = array("kel"=>"Kelvin","toa"=>"Toaster","lof"=>"Lo-Fi");
-        $filtersMQ = new MatchQuery("filter", $filters[$request->get("filter")]);
-
-        $catsBQ = new BoolQuery();
-        $catsBQ->addParameter("minimum_should_match", 1);
-        $catsBQ->addParameter("boost", 2);
-
-        foreach ($this->categories as $code=>$label) {
-            $request->get($code) && $catsBQ->add($this->makeMQ('categories', $label, $code), BoolQuery::SHOULD);
-        }
-
-        $daysBQ = new BoolQuery();
-        $daysBQ->addParameter("minimum_should_match", 1);
-        $daysBQ->addParameter("boost", 1);
-
-        foreach ($this->weekdays as $code=>$label) {
-            $request->get($code) && $daysBQ->add($this->makeMQ('days', $label, $code), BoolQuery::SHOULD);
-        }
-
-        $search->addQuery($filtersMQ);
-        $search->addQuery($catsBQ);
-        $search->addQuery($daysBQ);
-
-        $queryArray = $search->toArray();
-
-        $results = $repo->execute($search,Result::RESULTS_RAW_ITERATOR);
-        $count = count($results);
-        return $this->render('match/result.html.twig', compact("queryArray","results","count"));
-    }
-
-    /**
-     * Make MatchQuery for Days.
-     *
-     * @param $val
-     * @param $name
-     * @return MatchQuery
-     */
-    private function makeDaysMQ($val, $name){
-        return $this->makeMQ('days',$val,$name);
-    }
-
-    /**
-     * Make MatchQuery for Categories.
-     *
-     * @param $val
-     * @param $name
-     * @return MatchQuery
-     */
-    private function makeCatsMQ($val, $name){
-        return $this->makeMQ('categories',$val,$name);
-    }
-
-    /**
-     * Make MatchQuery for $type.
-     *
-     * @param $type
-     * @param $val
-     * @param $name
-     * @return MatchQuery
-     */
-    private function makeMQ($type, $val, $name) {
-        $mq = new MatchQuery($type.".name",$val);
-        $mq->addParameter("_name",$name);
-        return $mq;
+        $matcher = new Matcher(
+            $this->get('es.manager'),
+            $this->getParameter('estest.items.weekdays')
+        );
+        return $this->render('match/result.html.twig', $matcher->match($request));
     }
 }
---
+++ src/AppBundle/Matcher/Matcher.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace AppBundle\Matcher;
+
+use ONGR\ElasticsearchBundle\Service\Manager;
+use Symfony\Component\HttpFoundation\Request;
+use ONGR\ElasticsearchDSL\Search;
+use ONGR\ElasticsearchDSL\Query\BoolQuery;
+use ONGR\ElasticsearchDSL\Query\MatchQuery;
+use ONGR\ElasticsearchBundle\Result\Result;
+
+
+class Matcher
+{
+    /**
+     * Categories array.
+     *
+     * @var array
+     */
+    private $categories = [
+        "neo" => "Neo",
+        "tri" => "Trinity",
+        "cyp" => "Cypher",
+        "mor" => "Morpheus",
+        "tan" => "Tank",
+    ];
+
+    /**
+     * Folters array.
+     *
+     * @var
+     */
+    private $filters;
+
+    /**
+     * Weekdays array.
+     *
+     * @var array
+     */
+    private $weekdays;
+
+    /**
+     * ES Manager.
+     *
+     * @var Manager
+     */
+    private $manager;
+
+    /**
+     * Construct a new matcher.
+     *
+     * @param Manager $manager
+     * @param array $weekdays
+     */
+    public function __construct(Manager $manager, array $weekdays)
+    {
+        $this->manager = $manager;
+        $this->weekdays = $weekdays;
+        $this->filters = array("kel" => "Kelvin", "toa" => "Toaster", "lof" => "Lo-Fi");
+    }
+
+    /**
+     * Return matches based on criteria in request.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function match(Request $request)
+    {
+        $search = new Search();
+
+        $search->addQuery(new MatchQuery("filter", $this->filters[$request->get("filter")]));
+        $search->addQuery($this->makeBQ($request,$this->categories,'categories',2));
+        $search->addQuery($this->makeBQ($request,$this->weekdays,'days',1));
+
+        $queryArray = $search->toArray();
+
+        $repo = $this->manager->getRepository('AppBundle:Item');
+        $results = $repo->execute($search, Result::RESULTS_RAW_ITERATOR);
+        $count = count($results);
+
+        return compact("queryArray", "results", "count");
+    }
+
+    /**
+     * Make BoolQuery for $type.
+     *
+     * @param Request $request
+     * @param array $items
+     * @param $type
+     * @param $boost
+     * @return BoolQuery
+     */
+    private function makeBQ(Request $request, array $items, $type, $boost)
+    {
+        $bq = new BoolQuery();
+        $bq->addParameter("minimum_should_match", 1);
+        $bq->addParameter("boost", $boost);
+
+        foreach ($items as $code => $label) {
+            $request->get($code) && $bq->add($this->makeMQ($type, $label, $code), BoolQuery::SHOULD);
+        }
+        return $bq;
+    }
+
+    /**
+     * Make MatchQuery for $type.
+     *
+     * @param $type
+     * @param $val
+     * @param $name
+     * @return MatchQuery
+     */
+    private function makeMQ($type, $val, $name)
+    {
+        $mq = new MatchQuery($type . ".name", $val);
+        $mq->addParameter("_name", $name);
+        return $mq;
+    }
+
+}
