9df953dc14f8caa3b08613f1dab149115c9afc19
muggezifter <muggezifter@gmail.com>
2016-06-17 17:35:28 +0200

    make match on categories

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,8 +3,15 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use ONGR\ElasticsearchDSL\Search;
+use ONGR\ElasticsearchDSL\Query\BoolQuery;
+use ONGR\ElasticsearchDSL\Query\MatchQuery;
+use ONGR\ElasticsearchBundle\Result\Result;
+use AppBundle\Document\Item;
 
 class DefaultController extends Controller
 {
@@ -17,5 +24,47 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @Route("/match", name="matchGet")
+     * @Method({"GET"})
+     */
+    public function matchGetAction(Request $request){
+        return $this->render('match/form.html.twig');
+    }
+
+    /**
+     * @Route("/match", name="matchPost")
+     * @Method({"POST"})
+     */
+    public function matchPostAction(Request $request)
+    {
+        $manager = $this->get('es.manager');
+        $repo = $manager->getRepository('AppBundle:Item');
+
+        $search = new Search();
+
+        $bool = new BoolQuery();
+        $bool->addParameter("minimum_should_match", 1);
+        $bool->addParameter("boost", 1);
+
+        $request->get("neo") && $bool->add($this->makeMQ("Neo", "neo"), BoolQuery::SHOULD);
+        $request->get("tri") && $bool->add($this->makeMQ("Trinity", "tri"), BoolQuery::SHOULD);
+        $request->get("cyp") && $bool->add($this->makeMQ("Cypher", "cyp"), BoolQuery::SHOULD);
+        $request->get("mor") && $bool->add($this->makeMQ("Morpheus", "mor"), BoolQuery::SHOULD);
+        $request->get("tan") && $bool->add($this->makeMQ("Tank", "tan"), BoolQuery::SHOULD);
+
+        $search->addQuery($bool);
+
+        $results = $repo->execute($search,Result::RESULTS_RAW_ITERATOR);
+        $count = count($results);
+        return $this->render('match/result.html.twig', compact("results","count"));
+    }
+
+    function makeMQ($val, $name) {
+        $mq = new MatchQuery("categories.name",$val);
+        $mq->addParameter("_name",$name);
+        return $mq;
     }
 }
